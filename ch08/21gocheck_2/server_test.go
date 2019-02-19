58f4cd0456a8149a72d39bdf05821bc8e4b8a876
KentaMasago <masagotty@gmail.com>
2019-02-19 22:10:35 +0900

    21 added

--- ch08/21gocheck_2/server_test.go
+++ ch08/21gocheck_2/server_test.go
@@ -4,12 +4,17 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	. "gopkg.in/check.v1"
 )
 
-type PostTestSuite struct{}
+type PostTestSuite struct {
+	mux    *http.ServeMux
+	post   *FakePost
+	writer *httptest.ResponseRecorder
+}
 
 func init() {
 	Suite(&PostTestSuite{})
@@ -17,14 +22,41 @@ func init() {
 
 func Test(t *testing.T) { TestingT(t) }
 
-func (s *PostTestSuite) TestHandleGet(c *C) {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/post/", handleRequest(&FakePost{}))
-	writer := httptest.NewRecorder()
+func (s *PostTestSuite) SetUpTest(c *C) {
+	s.post = &FakePost{}
+	s.mux = http.NewServeMux()
+	s.mux.HandleFunc("/post/", handleRequest(s.post))
+	s.writer = httptest.NewRecorder()
+}
+
+func (s *PostTestSuite) TearDownTest(c *C) {
+	c.Log("Finished test - ", c.TestName())
+}
+
+func (s *PostTestSuite) SetUpSuite(c *C) {
+	c.Log("Starting Post Test Suite")
+}
+
+func (s *PostTestSuite) TearDownSuite(c *C) {
+	c.Log("Finishing Post Test Suite")
+}
+
+func (s *PostTestSuite) TestGetPost(c *C) {
 	request, _ := http.NewRequest("GET", "/post/1", nil)
-	mux.ServeHTTP(writer, request)
-	c.Check(writer.Code, Equals, 200)
+	s.mux.ServeHTTP(s.writer, request)
+
+	c.Check(s.writer.Code, Equals, 200)
 	var post Post
-	json.Unmarshal(writer.Body.Bytes(), &post)
+	json.Unmarshal(s.writer.Body.Bytes(), &post)
 	c.Check(post.Id, Equals, 1)
+}
+
+func (s *PostTestSuite) TestPutPost(c *C) {
+	json := strings.NewReader(`{"content":"Updated post","author":"Gotty"}`)
+	request, _ := http.NewRequest("PUT", "/post/1", json)
+	s.mux.ServeHTTP(s.writer, request)
+
+	c.Check(s.writer.Code, Equals, 200)
+	c.Check(s.post.Id, Equals, 1)
+	c.Check(s.post.Content, Equals, "Updated post")
 }
